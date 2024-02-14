5ec3840a03fcb107d8abf8f7e576d17b0333b2c0
Marvin Baga <mjdbaga@gmail.com>
2024-02-14 17:01:49 +0800

    feat: test db connection and add new user

--- src/index.php
+++ src/index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Start project";
+require_once './vendor/autoload.php';
+
+$host = $_ENV["MYSQL_HOST"];
+$db = $_ENV["MYSQL_DATABASE"];
+$user = $_ENV["MYSQL_USER"];
+$pw = $_ENV["MYSQL_PASSWORD"];
+
+$db = new MysqliDb($host, $user, $pw, $db);
+
+echo "test";
+
+$data = [
+    "name" => "Marvin Jayson",
+    "last_name" => "Baga",
+    "status" => "active"
+];
+
+$id = $db->insert('subscribers', $data);
+
+if($id)
+    echo 'user was created. Id=' . $id;
