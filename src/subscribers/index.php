269f2decb0f3c3aeae614017e26a835824b5d5c7
Marvin Baga <mjdbaga@gmail.com>
2024-02-15 16:24:11 +0800

    feat: implement api request for all subscribers - paginated response

--- src/subscribers/index.php
+++ src/subscribers/index.php
@@ -4,4 +4,23 @@ require_once "../config.php";
 require_once base_dir_path() . '/vendor/autoload.php';
 require_once base_dir_path() . '/app/db-connect.php';
 
-// list subscribers and add a form to insert a subs
+use SuscriberAPI\Models\Subscriber;
+
+$submodel = new Subscriber($db);
+
+$page = isset( $_GET['page'] ) ? $_GET['page'] : 1 ;
+
+$db->pageLimit = 10;
+$subscribers = $db->arraybuilder()->paginate("subscribers", $page);
+
+$result = [
+    "per_page" => 10,
+    "totalPages" => $db->totalPages,
+    "total" => (int) $db->totalCount,
+    "current_page" => $page,
+    "data" => $subscribers,
+];
+
+header('HTTP/1.1 200 OK');
+echo json_encode($result);
+exit();
